bd03ae4771796b5db69ffec408f1f3118ced2249
codewithmayor <mayor.michael19@gmail.com>
2022-12-07 23:05:22 +0200

    Task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if len(set_1.intersection(set_2)) > 0:
-        return (set_1.intersection(set_2))
+    if set_1 is None or set_2 is None:
+        return set()
+    return set_1 & set_2
