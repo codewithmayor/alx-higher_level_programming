f6452d099464a1d65ca8a3143a446f8126aecba6
codewithmayor <mayor.michael19@gmail.com>
2023-01-03 16:06:40 +0200

    Task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -6,7 +6,6 @@ Defines a class rectangle
 
 class Rectangle:
     """Definition of a rectangle"""
-
     def __init__(self, width=0, height=0):
         """Initializes the rectangle"""
         self.height = height
@@ -39,3 +38,13 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """returns the area"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """returns the perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return ((self.__width * 2) + (self.__height * 2))
