a9dba114f95b9b97fe34d252b395a78e2b9fbe03
codewithmayor <mayor.michael19@gmail.com>
2023-01-10 09:49:16 +0200

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module for class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+    def __init__(self):
+        """new instance of BaseGeometry"""
+        pass
+
+    def area(self):
+        """Public instance method that raise an exception"""
+        raise Exception("area() is not implemented")
