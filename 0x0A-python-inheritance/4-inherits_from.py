1361d017482a3528fea8c6367794bef6f2b15855
codewithmayor <mayor.michael19@gmail.com>
2023-01-10 09:47:28 +0200

    Task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module for function inherits_from"""
+
+
+def inherits_from(obj, a_class):
+    """
+    function that returns True if the object is an instance
+    of a class that inherited (directly or indirectly) from the
+    specified class ; otherwise False
+    """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
