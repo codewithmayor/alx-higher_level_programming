3e513b4f3efdbbf7bf706a4b4a5b16aee8a1c1c1
codewithmayor <mayor.michael19@gmail.com>
2023-02-14 23:29:28 +0200

    Task 4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- a script that creates a table called first_table in the current database in your MySQL server
+CREATE A TABLE IF NOT EXISTS `first_table` (
+	`id` int NOT NULL,
+	`name` varchar(256)
+	)
